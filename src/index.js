c67af56a434ef41ec005530310d47f2d95eb69a8
PraveenNarwal <praveennarwal5@gmail.com>
2021-06-11 18:11:00 +0530

    checkpoint 4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import Home from './screens/home/Home';
+import Details from './screens/details/Details';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>
-      <Home />
-    </div>
+  <><BrowserRouter>
 
-  </React.StrictMode>,
+    <Route path="/" component={Home} exact />
+    <Route path="/details/:id" component={Details} />
+  </BrowserRouter>
+  </>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 import Header from "../../common/header/Header";
 import GridListBanner from "../../common/GridListBanner";
@@ -16,6 +16,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Select from '@material-ui/core/Select';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
 
 import moviesData from "../../common/moviesData";
 import genre from "../../common/genre";
@@ -23,7 +24,39 @@ import artists from "../../common/artists";
 
 
 class Home extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = { movies: [], searchTerm: '' };
+    }
+
+    componentDidMount() {
+        this.setState({ movies: moviesData })
+    }
+
+    setSearch(search) {
+        this.setState({ searchTerm: search })
+    }
+
+
+    handleSubmit(artistName, genres) {
+        // console.log(artistName, genres);
+
+        this.setState({
+            movies: moviesData.filter(m => {
+                console.log("check", m.artists.find((a) => artistName.includes(a.first_name)));
+
+                return (m.artists.find((a) => artistName.includes(a.first_name)) !== undefined || m.title.toLowerCase().includes(this.state.searchTerm))
+            })
+        })
+        console.log("checking", this.movies);
+    }
+
     render() {
+
+        // console.log(this.state)
+
         return (
             <div>
                 <Header />
@@ -33,10 +66,10 @@ class Home extends React.Component {
                 <GridListBanner />
                 <div className="flex-container">
                     <div className="left">
-                        <Movielistdisplay />
+                        <Movielistdisplay movies={this.state.movies} />
                     </div>
                     <div className="right">
-
+                        <MovieFilter setSearch={this.setSearch.bind(this)} handleSubmit={this.handleSubmit.bind(this)} movies={this.state.movies} />
                     </div>
                 </div>
 
@@ -73,7 +106,7 @@ const styleForMovies = makeStyles((theme) => ({
 }));
 
 
-export function Movielistdisplay() {
+export function Movielistdisplay({ movies }) {
     const classess = styleForMovies();
 
     return (
@@ -82,18 +115,10 @@ export function Movielistdisplay() {
                 <GridListTile key="Subheader" cols={4} style={{ height: 'auto' }}>
 
                 </GridListTile>
-                {/* .filter((val) => {
-                    if (searchTerm === "") {
-                        return val;
-                    } else if (
-                        val.title.toLowerCase().includes(searchTerm.toLowerCase())
-                    ) {
-                        return val
-                    }
-                }) */}
-                {moviesData.map((tile) => (
+
+                {movies.map((tile) => (
                     <GridListTile key={tile.poster_url}>
-                        <img src={tile.poster_url} alt={tile.title} style={{ cursor: 'pointer' }} />
+                        <Link to={`/details/${tile.id}`}><img src={tile.poster_url} alt={tile.title} style={{ cursor: 'pointer' }} /></Link>
                         <GridListTileBar
                             title={tile.title}
                             subtitle={<span>{tile.release_date}</span>}
@@ -111,8 +136,160 @@ export function Movielistdisplay() {
 
 
 
+
 // ----------------------filter list apply -----------------------------------------------------------------
 // ----------------------filter list apply -----------------------------------------------------------------
+
+
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240,
+
+    },
+
+    title: {
+        color: theme.palette.primary.light,
+        margin: theme.spacing.unit,
+
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 200,
+
+    },
+
+}));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+
+export function MovieFilter({ setSearch, handleSubmit, setArtist }) {
+    const classes = useStyles();
+    const [artistName, setArtistName] = React.useState([]);
+    const [genres, setGenres] = React.useState([]);
+
+    const handleChange = (event) => {
+        setArtistName(event.target.value);
+    };
+    // console.log(artistName);
+
+    // search the movies ---------------------------------------------------------------------------------
+
+
+
+
+    return (
+
+        <Card className={classes.root}>
+            <label className={classes.title}>FIND MOVIES BY:</label>
+            <FormControl className={classes.root}>
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField id="standard-basic" label="Movie Name" onChange={(e) => setSearch(e.target.value)} />
+                </form>
+
+                <div>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-mutiple-checkbox-label">Genres</InputLabel>
+                        <Select
+                            labelId="demo-mutiple-checkbox-label"
+                            id="demo-mutiple-checkbox"
+                            multiple
+                            value={genres}
+                            onChange={(e) => setGenres(e.target.value)}
+                            input={<Input />}
+                            renderValue={(selected) => selected.join(', ')}
+                            MenuProps={MenuProps}
+                        >
+                            {genre.map((name) => (
+                                <MenuItem key={name.name} value={name.name}>
+                                    <Checkbox checked={genres.indexOf(name.name) > -1} />
+                                    <ListItemText primary={name.name} />
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </div>
+
+                <div>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-mutiple-checkbox-label">Artists</InputLabel>
+                        <Select
+                            // onChange={(e) => e.target.value)}
+                            labelId="demo-mutiple-checkbox-label"
+                            id="demo-mutiple-checkbox"
+                            multiple
+                            value={artistName}
+                            onChange={handleChange}
+                            input={<Input />}
+                            renderValue={(selected) => selected.join(', ')}
+                            MenuProps={MenuProps}
+                        >
+                            {artists.map((name) => (
+                                <MenuItem key={name.first_name} value={name.first_name}>
+                                    <Checkbox checked={artistName.indexOf(name.first_name) > -1} />
+                                    <ListItemText primary={name.first_name} />
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </div>
+
+                <div>
+                    <FormControl className={classes.formControl}>
+                        <TextField
+                            id="date"
+                            label="Release Date Start"
+                            type="date"
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </FormControl>
+                </div>
+
+                <div>
+                    <FormControl className={classes.formControl}>
+                        <TextField
+                            id="date"
+                            label="Release Date End"
+                            type="date"
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </FormControl>
+                </div>
+                <div className={classes.root}>
+                    <Button className={classes.formControl} variant="contained" color="primary" onClick={() => handleSubmit(artistName, genres)} >Apply</Button>
+                </div>
+
+
+            </FormControl>
+
+
+        </Card>
+
+    );
+}
+
 
 
 
